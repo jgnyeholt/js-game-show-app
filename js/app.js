868dfbbf2b8205e590bdc09af601917c18c2626a
Jonathan <jgnyeholt@gmail.com>
2019-08-15 20:54:16 -0400

    Update App.js
    Added phrases

--- js/app.js
+++ js/app.js
@@ -2,7 +2,23 @@ const keyboard = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const overlay = document.getElementById("overlay");
 const resetButton = document.querySelector("#overlay .btn__reset");
-
+const phrases = [
+  "A Chip on Your Shoulder",
+  "A Dime a Dozen",
+  "A Fool and His Money Are Soon Parted",
+  "A Piece of Cake",
+  "An Arm and a Leg",
+  "Back to Square One",
+  "Barking Up the Wrong Tree",
+  "Beating Around the Bush",
+  "Between a Rock and a Hard Place",
+  "Burst Your Bubble",
+  "Close But No Cigar",
+  "Cry Over Spilled Milk",
+  "Cup of Joe",
+  "Curiosity Killed the Cat",
+  "Cut to the Chase"  
+];
 let missed = 0;
 
 //remove overlay when start game is clicked
